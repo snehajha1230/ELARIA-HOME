4557372e13e939cf1560a0e28be522623f6fd952
SNEHA JHA <snehajha1230@gmail.com>
2025-08-11 17:27:31 +0530

    Update ResetPassword.jsx

--- client/src/components/ResetPassword.jsx
+++ client/src/components/ResetPassword.jsx
@@ -20,7 +20,7 @@ const ResetPassword = () => {
 
     setIsLoading(true);
     try {
-      await axios.post('http://localhost:5000/api/auth/reset-password', {
+      await axios.post('https://elaria-server.onrender.com/api/auth/reset-password', {
         token,
         password
       });
