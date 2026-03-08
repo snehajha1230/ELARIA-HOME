acb6f6252889556e4c306170a3c80b76c7ae71cd
SNEHA JHA <snehajha1230@gmail.com>
2026-03-08 14:28:44 +0530

    Update api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5001/api',
+  baseURL: 'https://elaria-server.onrender.com/api',
 });
 
 instance.interceptors.request.use(
